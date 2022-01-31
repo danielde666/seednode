0f113341983e0f407d7a303a1a29e3a4835c04c1
danny de <danielde666@users.noreply.github.com>
2022-01-31 13:38:21 -1000

    copy and style fix

--- pages/index.js
+++ pages/index.js
@@ -185,7 +185,7 @@ function DiscountExample({ signer }) {
 		<>
 			<p className="prices">PRICE: {discountHolder === null ? "$2500.00" : <><strike>$2500.00</strike> <span>$1250.00</span></>}</p>
 			<p>SEED NFT owners - Receive a 50% discount.</p>
-			<button onClick={checkForDiscount}>{discountHolder === null ? "Verify for Discount" : "Discunt Applied!"}</button>
+			<button onClick={checkForDiscount}>{discountHolder === null ? "Verify for Discount" : "Discount Applied!"}</button>
 			<ul>
 				<li>
 					<b>Discount:</b> {discountHolder === null ? "unknown" : `${discountHolder}`}
