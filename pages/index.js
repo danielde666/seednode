a88fee10db4224d5db90610bd1a507e8cb4cb8e4
danny de <danielde666@users.noreply.github.com>
2022-02-04 16:44:37 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -228,7 +228,7 @@ const Index = ({ product }) => {
 
 					<>
 						<p>{product.title}(s)<br></br>
-							Lucien Smith<br></br>Acrylic paint on canvas<br></br> 8 x 10 in<br></br> 20.32 x 25.4 cm
+							Lucien Smith<br></br>Acrylic paint and silkscreen ink on canvas<br></br> 8 x 10 in<br></br> 20.32 x 25.4 cm
 						</p>
 					</>
 				
