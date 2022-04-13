c5533283b27c123a1dc11cb2888d2738dc0da89f
danny de <danielde666@users.noreply.github.com>
2022-04-12 23:01:01 -0700

    heisei-kaku-gothic-std

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,11 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
+import 'https://use.typekit.net/llx7cee.css';
+
+  
+
+
 const { Row, Column } = Grid;
 
 function DiscountExample({ signer }) {
